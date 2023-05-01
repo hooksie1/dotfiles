c95019ca837000b6fe5a00b6f6981237da1da292
John Hooks <hooksie11@gmail.com>
2023-04-30 20:46:04 -0400

    Fixes

--- nvim/lua/hooksie1/remap.lua
+++ nvim/lua/hooksie1/remap.lua
@@ -1,11 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":Neotree reveal<cr>")
 
--- toggleterm
-vim.keymap.set("n", "<C-t>", ":ToggleTerm size=40 dir=. hidden=true direction=float<cr>")
-vim.keymap.set("t", "<C-t>", [[<C-\><C-N>:ToggleTerm size=40 dir=. hidden=true direction=float<cr>]])
-vim.keymap.set("i", "<C-t>", [[<C-\><C-N>:ToggleTerm size=40 dir=. hidden=true direction=float<cr>]])
-
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
