030a32cfd53a923e02992163bd2139541e463fb4
John Hooks <hooksie11@gmail.com>
2023-07-05 09:54:06 -0400

    updates

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -31,14 +31,12 @@ lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n",  "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n",  "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 	vim.keymap.set("n",  "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n",  "[d", function() vim.diagnostic.goto_next() end, opts)
 	vim.keymap.set("n",  "]d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n",  "<leader>vca", function() vim.diagnostic.code_action() end, opts)
-	vim.keymap.set("n",  "<leader>vrr", function() vim.diagnostic.references() end, opts)
-	vim.keymap.set("n",  "<leader>vrn", function() vim.diagnostic.rename() end, opts)
 end)
 
 lsp.format_on_save({
