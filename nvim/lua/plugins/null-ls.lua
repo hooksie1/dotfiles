9c3ca1cbf334e1e079a8f1d9154530099cfcd6de
John Hooks <hooksie11@gmail.com>
2024-01-19 09:19:28 -0500

    add autoformatting

---
+++ nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.goimports,
+        },
+      }
+    end,
+  },
+}
