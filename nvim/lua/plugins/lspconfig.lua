3dbaa39e124b248f5123ad25bf521b16fa48e3bf
John Hooks <hooksie11@gmail.com>
2024-02-02 09:12:08 -0500

    add stuff

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,10 +5,21 @@ return {
       servers = {
         gopls = {},
         zls = {},
-        v_analyzer = {},
+        v_analyzer = {
+          default_config = {
+            filetypes = { "v" },
+          }
+        },
         rls = {},
         lua_ls = {},
       },
     },
   },
+  {
+    "kitagry/regols",
+    default_config = {
+      cmd = { 'regols' },
+      filetypes = { 'rego' }
+    }
+  }
 }
