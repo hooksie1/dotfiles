222997d80f025d52d912b61938af74bfd220bb6e
John Hooks <hooksie11@gmail.com>
2024-02-02 09:12:08 -0500

    add stuff

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+return {
+  "L3MON4D3/LuaSnip",
+  opts = {
+    history = true,
+    region_check_events = "InsertEnter",
+    delete_check_events = "TextChanged",
+  },
+}
