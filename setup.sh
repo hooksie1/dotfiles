e1ef9a4ec2a7b17a8966fa54a94a74eb0251c9d0
John Hooks <hooksie11@gmail.com>
2024-12-02 15:39:00 -0500

    add ghostty config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.config/{zellij,alacritty}
+mkdir -p ~/.config/{zellij,alacritty,ghostty}
 
 cp starship.toml ~/.config/starship.toml
 
@@ -11,3 +11,4 @@ ln -sf $(pwd)/zellij.kdl ~/.config/zellij/config.kdl
 ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf $(pwd)/wezterm.lua ~/.wezterm.lua
 ln -sf $(pwd)/aerospace.toml ~/.aerospace.toml
+ln -sf $(pwd)/ghostty ~/.config/ghostty/config
