884ecfc115ccad153d8ee356af40141a38feb147
John Hooks <hooksie11@gmail.com>
2024-12-26 13:31:32 -0500

    update keybindings and ghostty

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
-      open_mapping = [[<C-t>]],
+      open_mapping = [[<C-space>t]],
       start_in_insert = true,
       size = 40,
       hidden = true,
