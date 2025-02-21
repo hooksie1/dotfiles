0e2a4d7ccc6bd59339e4240406e34634bd3bbb3f
John Hooks <hooksie11@gmail.com>
2025-02-21 16:46:59 -0500

    updates

--- nvim/lua/custom/plugins/toggleterm.lua
+++ nvim/lua/custom/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
     'akinsho/toggleterm.nvim',
     version = '*',
     opts = {
-      open_mapping = [[<C-space>t]],
+      open_mapping = [[<C-y>]],
       start_in_insert = true,
       size = 40,
       hidden = true,
