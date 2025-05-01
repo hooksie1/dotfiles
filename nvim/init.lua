9b6098070290f5a26be73e6fb3c597c56ec72f30
John Hooks <john.hooks@chainguard.dev>
2025-05-01 14:43:32 -0400

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -675,6 +675,7 @@ require('lazy').setup({
       },
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'goimports' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
