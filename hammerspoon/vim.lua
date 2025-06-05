414cb16b3cdc8a5f3bbbaea784c319f51fc92a7c
John Hooks <hooksie11@gmail.com>
2025-06-05 18:28:50 -0400

    update variable name

--- hammerspoon/vim.lua
+++ hammerspoon/vim.lua
@@ -1,19 +1,19 @@
 --  set up vim motions
-directions = {
-    ["h"] = "left",
-    ["j"] = "down",
-    ["k"] = "up",
-    ["l"] = "right"
+DIRECTIONS = {
+	["h"] = "left",
+	["j"] = "down",
+	["k"] = "up",
+	["l"] = "right",
 }
 
 local function keyCode(key, modifiers)
-    modifiers = modifiers or {}
-    return function()
-        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-        hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
-    end
+	modifiers = modifiers or {}
+	return function()
+		hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+		hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+	end
 end
 
-for key, direction in pairs(directions) do
-    hs.hotkey.bind({ "cmd", "ctrl" }, key, keyCode(direction), nil, keyCode(direction))
+for key, direction in pairs(DIRECTIONS) do
+	hs.hotkey.bind({ "cmd", "ctrl" }, key, keyCode(direction), nil, keyCode(direction))
 end
