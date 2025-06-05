c46fc5494404effa7ba542cf62f3742e55a472ce
John Hooks <john.hooks@chainguard.dev>
2025-06-05 17:04:19 -0400

    move hammerspoon to directory

---
+++ hammerspoon/init.lua
@@ -0,0 +1,7 @@
+hs.loadSpoon("ReloadConfiguration")
+HYPER = { "cmd", "ctrl" }
+
+require 'apps'
+require 'vim'
+
+spoon.ReloadConfiguration:start()
---
+++ hammerspoon/testing.lua
@@ -0,0 +1,12 @@
+local function showTitle()
+    local window = hs.window.focusedWindow()
+    hs.alert.show(window:application():title())
+end
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "y", showTitle, nil, nil)
+
+hs.urlevent.bind("someAlert", function(eventName, params)
+    for k, v in pairs(params) do
+        hs.alert.show(k .. " = " .. v)
+    end
+end)
