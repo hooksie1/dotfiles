bdbb577fe7fb9168cd9227072cb1ae6d7d670eed
John Hooks <hooksie11@gmail.com>
2025-06-05 18:26:53 -0400

    update apps

--- hammerspoon/apps.lua
+++ hammerspoon/apps.lua
@@ -1,19 +1,20 @@
 APPS = {
-    ["g"] = "com.mitchellh.ghostty",
-    ["z"] = "app.zen-browser.zen",
-    ["o"] = "md.obsidian",
-    ["s"] = "com.tinyspeck.slackmacgap",
-    ["d"] = "dev.zed.Zed",
+	["g"] = "com.mitchellh.ghostty",
+	["z"] = "app.zen-browser.zen",
+	["o"] = "md.obsidian",
+	["s"] = "com.tinyspeck.slackmacgap",
+	["d"] = "dev.zed.Zed",
+	["t"] = "ru.keepcoder.Telegram",
 }
 
 local function activate(name)
-    local apps = hs.application.applicationsForBundleID(name)
-    local app = apps[1]
-    app:activate()
+	local apps = hs.application.applicationsForBundleID(name)
+	local app = apps[1]
+	app:activate()
 end
 
 for key, app in pairs(APPS) do
-    hs.hotkey.bind(HYPER, key, function()
-        activate(app)
-    end)
+	hs.hotkey.bind(HYPER, key, function()
+		activate(app)
+	end)
 end
