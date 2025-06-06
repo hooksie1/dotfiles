bc05a77864768e999305ad4de404a931366a70c9
John Hooks <john.hooks@chainguard.dev>
2025-06-06 12:13:09 -0400

    update formatting

--- hammerspoon/window_movement.lua
+++ hammerspoon/window_movement.lua
@@ -1,43 +1,45 @@
 local function mover(unit)
-    return function(win)
-        win:move(unit, nil, true)
-    end
+	return function(win)
+		win:move(unit, nil, true)
+	end
 end
 
 local layouts = {
-    topLeft = { x = 0, y = 0, w = 0.5, h = 0.5 },
-    topRight = { x = 0.5, y = 0, w = 0.5, h = 0.5 },
-    bottomLeft = { x = 0, y = 0.5, w = 0.5, h = 0.5 },
-    bottomRight = { x = 0.5, y = 0.5, w = 0.5, h = 0.5 },
-    leftHalf = { x = 0, y = 0, w = 0.5, h = 1 },
-    rightHalf = { x = 0.5, y = 0, w = 0.5, h = 1 },
+	topLeft = { x = 0, y = 0, w = 0.5, h = 0.5 },
+	topRight = { x = 0.5, y = 0, w = 0.5, h = 0.5 },
+	bottomLeft = { x = 0, y = 0.5, w = 0.5, h = 0.5 },
+	bottomRight = { x = 0.5, y = 0.5, w = 0.5, h = 0.5 },
+	leftHalf = { x = 0, y = 0, w = 0.5, h = 1 },
+	rightHalf = { x = 0.5, y = 0, w = 0.5, h = 1 },
 }
 
 local positions = {
-    maximize = function(win) win:maximize() end
+	maximize = function(win)
+		win:maximize()
+	end,
 }
 
 local bindings = {
-    ["j"] = "bottomLeft",
-    ["k"] = "bottomRight",
-    ["u"] = "topLeft",
-    ["i"] = "topRight",
-    ["Left"] = "leftHalf",
-    ["Right"] = "rightHalf",
-    ["Return"] = "maximize",
+	["j"] = "bottomLeft",
+	["k"] = "bottomRight",
+	["u"] = "topLeft",
+	["i"] = "topRight",
+	["Left"] = "leftHalf",
+	["Right"] = "rightHalf",
+	["Return"] = "maximize",
 }
 
 -- build functions for positions with layout table and mover
 for name, layout in pairs(layouts) do
-    positions[name] = mover(layout)
+	positions[name] = mover(layout)
 end
 
 -- bind keys in bindings to positions
 for key, position in pairs(bindings) do
-    hs.hotkey.bind(HYPER, key, function()
-        local win = hs.window.focusedWindow()
-        if win then
-            positions[position](win)
-        end
-    end)
+	hs.hotkey.bind(HYPER, key, function()
+		local win = hs.window.focusedWindow()
+		if win then
+			positions[position](win)
+		end
+	end)
 end
