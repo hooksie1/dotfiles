3df41b130d4296626be50bb900eb778c1b645a96
John Hooks <john.hooks@chainguard.dev>
2025-06-06 11:20:24 -0400

    abstract some more because why not

--- hammerspoon/window_movement.lua
+++ hammerspoon/window_movement.lua
@@ -1,39 +1,41 @@
+local function mover(unit)
+    return function(win)
+        win:move(unit, nil, true)
+    end
+end
+
+local layouts = {
+    topLeft = { x = 0, y = 0, w = 0.5, h = 0.5 },
+    topRight = { x = 0.5, y = 0, w = 0.5, h = 0.5 },
+    bottomLeft = { x = 0, y = 0.5, w = 0.5, h = 0.5 },
+    bottomRight = { x = 0.5, y = 0.5, w = 0.5, h = 0.5 },
+    leftHalf = { x = 0, y = 0, w = 0.5, h = 1 },
+    rightHalf = { x = 0.5, y = 0, w = 0.5, h = 1 },
+}
+
 local positions = {
-    topLeft = function(win)
-        win:move({ x = 0, y = 0, w = 0.5, h = 0.5 })
-    end,
-    topRight = function(win)
-        win:move({ x = 0.5, y = 0, w = 0.5, h = 0.5 })
-    end,
-    bottomLeft = function(win)
-        win:move({ x = 0, y = 0.5, w = 0.5, h = 0.5 })
-    end,
-    bottomRight = function(win)
-        win:move({ x = 0.5, y = 0.5, w = 0.5, h = 0.5 })
-    end,
-    leftHalf = function(win)
-        win:move({ x = 0, y = 0, w = 0.5, h = 1 })
-    end,
-    rightHalf = function(win)
-        win:move({ x = 0.5, y = 0, w = 0.5, h = 1 })
-    end,
-    maximize = function(win) win:maximize() end,
+    maximize = function(win) win:maximize() end
 }
 
-local keys = {
-    ["j"] = positions.bottomLeft,
-    ["k"] = positions.bottomRight,
-    ["u"] = positions.topLeft,
-    ["i"] = positions.topRight,
-    ["Left"] = positions.leftHalf,
-    ["Right"] = positions.rightHalf,
-    ["Return"] = positions.maximize,
+local bindings = {
+    ["j"] = "bottomLeft",
+    ["k"] = "bottomRight",
+    ["u"] = "topLeft",
+    ["i"] = "topRight",
+    ["Left"] = "leftHalf",
+    ["Right"] = "rightHalf",
+    ["Return"] = "maximize",
 }
 
-for key, position in pairs(keys) do
+for name, layout in pairs(layouts) do
+    positions[name] = mover(layout)
+end
+
+for key, position in pairs(bindings) do
     hs.hotkey.bind(HYPER, key, function()
         local win = hs.window.focusedWindow()
-        if not win then return end
-        position(win)
+        if win then
+            positions[position](win)
+        end
     end)
 end
