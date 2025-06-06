df15aa23f3f17b693691361b50b8c5ac35f02fb5
John Hooks <john.hooks@chainguard.dev>
2025-06-06 11:08:02 -0400

    simplify window movement functions

--- hammerspoon/window_movement.lua
+++ hammerspoon/window_movement.lua
@@ -1,10 +1,22 @@
 local positions = {
-    topLeft = { x = 0, y = 0, w = 0.5, h = 0.5 },
-    topRight = { x = 0.5, y = 0, w = 0.5, h = 0.5 },
-    bottomLeft = { x = 0, y = 0.5, w = 0.5, h = 0.5 },
-    bottomRight = { x = 0.5, y = 0.5, w = 0.5, h = 0.5 },
-    leftHalf = { x = 0, y = 0, w = 0.5, h = 1 },
-    rightHalf = { x = 0.5, y = 0, w = 0.5, h = 1 },
+    topLeft = function(win)
+        win:move({ x = 0, y = 0, w = 0.5, h = 0.5 })
+    end,
+    topRight = function(win)
+        win:move({ x = 0.5, y = 0, w = 0.5, h = 0.5 })
+    end,
+    bottomLeft = function(win)
+        win:move({ x = 0, y = 0.5, w = 0.5, h = 0.5 })
+    end,
+    bottomRight = function(win)
+        win:move({ x = 0.5, y = 0.5, w = 0.5, h = 0.5 })
+    end,
+    leftHalf = function(win)
+        win:move({ x = 0, y = 0, w = 0.5, h = 1 })
+    end,
+    rightHalf = function(win)
+        win:move({ x = 0.5, y = 0, w = 0.5, h = 1 })
+    end,
     maximize = function(win) win:maximize() end,
 }
 
@@ -22,11 +34,6 @@ for key, position in pairs(keys) do
     hs.hotkey.bind(HYPER, key, function()
         local win = hs.window.focusedWindow()
         if not win then return end
-
-        if type(position) == "function" then
-            position(win)
-        elseif type(position) == "table" then
-            win:move(position, nil, true, 0)
-        end
+        position(win)
     end)
 end
