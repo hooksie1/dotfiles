690b0bbb55acfa958d4f518d746889fe10aeb887
John Hooks <hooksie11@gmail.com>
2025-06-07 21:30:45 -0400

    update

--- hammerspoon/window_movement.lua
+++ hammerspoon/window_movement.lua
@@ -1,9 +1,4 @@
-local function mover(unit)
-	return function(win)
-		win:move(unit, nil, true)
-	end
-end
-
+-- tables for layouts
 local layouts = {
 	topLeft = { x = 0, y = 0, w = 0.5, h = 0.5 },
 	topRight = { x = 0.5, y = 0, w = 0.5, h = 0.5 },
@@ -11,12 +6,8 @@ local layouts = {
 	bottomRight = { x = 0.5, y = 0.5, w = 0.5, h = 0.5 },
 	leftHalf = { x = 0, y = 0, w = 0.5, h = 1 },
 	rightHalf = { x = 0.5, y = 0, w = 0.5, h = 1 },
-}
-
-local positions = {
-	maximize = function(win)
-		win:maximize()
-	end,
+	bottomHalf = { x = 0, y = 0.5, w = 1, h = 0.5 },
+	topHalf = { x = 0, y = 0, w = 1, h = 0.5 },
 }
 
 local bindings = {
@@ -26,12 +17,27 @@ local bindings = {
 	["i"] = "topRight",
 	["Left"] = "leftHalf",
 	["Right"] = "rightHalf",
+	["Down"] = "bottomHalf",
+	["Up"] = "topHalf",
 	["Return"] = "maximize",
 }
 
+local positions = {
+	maximize = function(win)
+		win:maximize()
+	end,
+}
+
+-- moverFunc constructs the functions to move windows from the layouts table
+local function moverFunc(unit)
+	return function(win)
+		win:move(unit, nil, true)
+	end
+end
+
 -- build functions for positions with layout table and mover
 for name, layout in pairs(layouts) do
-	positions[name] = mover(layout)
+	positions[name] = moverFunc(layout)
 end
 
 -- bind keys in bindings to positions
