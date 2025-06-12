cae21e5b8b53fc9028195c6166e37031b1474e8d
John Hooks <john.hooks@chainguard.dev>
2025-06-12 09:57:34 -0400

    add clipboard

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,10 +1,12 @@
 hs.loadSpoon("ReloadConfiguration")
 HYPER = { "cmd", "ctrl" }
 HYPERWINDOW = { "cmd", "ctrl", "shift" }
+HYPERCLIPBOARD = { "alt", "ctrl" }
 
 require("apps")
 require("vim")
 require("texting")
 require("window_movement")
+require("clipboard")
 
 spoon.ReloadConfiguration:start()
